7aa578daef4d449b0ed8b407931566b4bafa554d
pawelkazubski <pawel.kazubski@idweaver.com>
2023-05-23 21:24:44 +0200

    vxda-tabs making dynamic orientation change

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,35 @@
     <!--        <VxdaTabsItem title="drugi item" id="test">qrwa mac 2</VxdaTabsItem>-->
     <!--    </VxdaTabs>-->
 
-    <VxdaTabs animation="fadeDown">
-        <VxdaTabsItem title="slot 1" v-slot="{ actionSlotAfter }">
+    <VxdaTabs animation="fadeDown" class="my-tabs" is-post-slot-active :breakpoints="breakpoints">
+        <VxdaTabsItem title="slot 1sadfasdfadsfasdfasdfasdfasf" v-slot="{ actionSlotAfter }" class="qrwamac asdas">
             <TestComp :action-slot-after="actionSlotAfter" class="qrwa"/>
         </VxdaTabsItem>
 
-        <VxdaTabsItem title=" tab number 2" costam="asd">
+        <VxdaTabsItem title=" tab number 2 asdfasf asdf asd fasd " costam="asd">
             <div>qrwa ,ac</div>
         </VxdaTabsItem>
 
         <div>qrwa</div>
 
-        <VxdaTabsItem title="slot 3" class="qrwa">
-            <div >
+        <VxdaTabsItem title="slot 3dsaf asdf asd f" class="qrwa">
+            <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+                aliquam aliquid architecto assumenda autem consectetur dolorem dolorum, id
+                impedit magni maxime, nemo officia quaerat quasi qui repellat, soluta
+                temporibus voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit. Accusamus aliquam aliquid architecto assumenda autem consectetur
+                dolorem dolorum, id impedit magni maxime, nemo officia quaerat quasi qui
+                repellat, soluta temporibus voluptatem. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Accusamus aliquam aliquid architecto
+                assumenda autem consectetur dolorem dolorum, id impedit magni maxime, nemo
+                officia quaerat quasi qui repellat, soluta temporibus voluptatem. Lorem
+                ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam
+                aliquid architecto assumenda autem consectetur dolorem dolorum, id impedit
+                magni maxime, nemo officia quaerat quasi qui repellat, soluta temporibus
+                voluptatem.
+            </div>
+            <div>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
                 aliquam aliquid architecto assumenda autem consectetur dolorem dolorum, id
                 impedit magni maxime, nemo officia quaerat quasi qui repellat, soluta
@@ -33,7 +49,7 @@
                 voluptatem.
             </div>
         </VxdaTabsItem>
-        <VxdaTabsItem title="slot 4"> content4</VxdaTabsItem>
+        <VxdaTabsItem title="slot 4 asdfadsfasdfasdfasdfasfasdfasdfas"> content4</VxdaTabsItem>
     </VxdaTabs>
 
 </template>
@@ -42,7 +58,24 @@
     import { ref } from 'vue';
     import { VxdaTabs, VxdaTabsItem } from '@/components';
     import TestComp from '@/components/TestComp/TestComp.vue';
+    import type { BreakpointsType } from '@/components/Tabs/Tabs.vue';
 
     const activeTabId = ref('asd');
     const activeTabId2 = ref('test');
+
+    const breakpoints: BreakpointsType = {
+        0: 'vertical',
+        560: 'horizontal',
+    };
 </script>
+
+
+<style>
+    body {
+    //background-color: black;
+    }
+
+    .my-tabs {
+    //height: 800px;
+    }
+</style>
--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -1,27 +1,46 @@
 <template>
     <div class="vxda-tabs" ref="$rootElement">
-        <div>{{ activeTabIdComputed }}</div>
-        <div v-if="mySlotItems?.length" class="vxda-tabs__wrapper">
-            <nav class="vxda-tabs__nav">
+        <div
+            v-if="currentOrientation === 'vertical' && (isPreSlotActive || isPostSlotActive)"
+            class="vxda-tabs__teleport-slots-vertical">
+            <div v-if="isPreSlotActive" class="vxda-tabs__slot-before"
+                 ref="teleportSlotBefore"
+                 :key="`before-${activeTabIdComputed}`"/>
 
-                <div class="vxda-tabs__slot-before" ref="teleportSlotBefore" :key="`before-${activeTabIdComputed}`"/>
+            <div v-if="isPostSlotActive" class="vxda-tabs__slot-after"
+                 ref="teleportSlotAfter"
+                 :key="`after-${activeTabIdComputed}`"
+                 :id="`after-${activeTabIdComputed}`"/>
+        </div>
 
-                <div class="vxda-tabs__buttons">
+        <div v-if="mySlotItems?.length" class="vxda-tabs__wrapper" :class="`vxda-tabs__wrapper--${currentOrientation}`">
+            <nav class="vxda-tabs__nav" :class="[gridStylesClassNames, `vxda-tabs__nav--${currentOrientation}`]">
+
+                <div v-if="isPreSlotActive && currentOrientation === 'horizontal'" class="vxda-tabs__slot-before"
+                     ref="teleportSlotBefore"
+                     :key="`before-${activeTabIdComputed}`"/>
+
+                <div class="vxda-tabs__buttons" :class="`vxda-tabs__buttons--${currentOrientation}`">
                     <button v-for="(button, index) in mySlotItems" :key="button.id"
-                            :class="{'vxda-tabs__buttons--active' : button.id === activeTabIdComputed}"
+                            :class="[{'vxda-tabs__button--active' : button.id === activeTabIdComputed}, `vxda-tabs__button--${currentOrientation}`]"
                             class="vxda-tabs__button"
                             type="button"
                             @click="setActiveTab(button.id)">
                         {{ button?.props?.title || index }}
+                        <span v-if="button.id === activeTabIdComputed"
+                              :class="`vxda-tabs__active-button-mask--${currentOrientation}`"
+                              class="vxda-tabs__active-button-mask"></span>
                     </button>
                 </div>
 
-                <div class="vxda-tabs__slot-after" ref="teleportSlotAfter" :key="`after-${activeTabIdComputed}`"
+                <div v-if="isPostSlotActive && currentOrientation === 'horizontal'" class="vxda-tabs__slot-after"
+                     ref="teleportSlotAfter"
+                     :key="`after-${activeTabIdComputed}`"
                      :id="`after-${activeTabIdComputed}`"/>
 
             </nav>
 
-            <div class="vxda-tabs__content">
+            <div class="vxda-tabs__content" :class="`vxda-tabs__content--${currentOrientation}`">
                 <div class="vxda-tabs__content-inner" ref="tabsContentInner">
 
                     <transition :name="animation">
@@ -30,7 +49,7 @@
                                 <component
                                     :is="activeTab"
                                     :id="activeTab.id"
-                                    :slot-id="activeTab.id"
+                                    :?slot-id="activeTab.id"
                                     :action-slot-before="teleportSlotBefore"
                                     :action-slot-after="teleportSlotAfter"/>
                             </keep-alive>
@@ -49,10 +68,20 @@
     import { useGetUniqueId } from '../../composables/useGetUniqueId';
     import './index.scss';
 
+    //todo add accordion
+    type OrientationTypes = 'vertical' | 'horizontal';
+    export type BreakpointsType = {
+        [key: number]: OrientationTypes
+    }
+
     type Props = {
         tabsAlignment?: 'center' | 'start' | 'end';
         activeTabId?: string;
-        animation?: 'fade' | 'fadeLeft' | 'fadeRight' | 'fadeDown' | 'fadeUp' | 'none'
+        animation?: 'fade' | 'fadeLeft' | 'fadeRight' | 'fadeDown' | 'fadeUp' | 'none';
+        isPreSlotActive?: boolean;
+        isPostSlotActive?: boolean;
+        orientation: OrientationTypes;
+        breakpoints?: BreakpointsType
     }
 
     defineOptions({
@@ -61,9 +90,12 @@
 
     const props = withDefaults(defineProps<Props>(), {
         tabsAlignment: 'center',
-        animation: 'fade'
+        animation: 'fade',
+        isPreSlotActive: false,
+        isPostSlotActive: false,
+        orientation: 'horizontal'
     });
-    const contentHeight = ref('0px');
+    const contentHeight = ref<number | string>('0');
     const tabsContentInner = ref<null | HTMLElement>(null);
     const slots = useSlots();
     const { getId } = useGetUniqueId();
@@ -74,6 +106,7 @@
         'update:activeTabId': [tabId: string]
     }>();
     const activeTabIdFallback = ref<string>('');
+    const currentOrientation = ref(props.orientation);
 
     const mySlotItems = computed(() => {
         const mySlots = slots.default?.().map((item) => ({
@@ -85,24 +118,68 @@
         return mySlots?.filter(({ type }) => type.hasOwnProperty('name') && type?.name === 'VxdaTabsItem') || [];
     });
 
-    console.log('mySlotItems', mySlotItems.value);
-    console.log(slots.default?.(), slots.default?.());
-
     const activeTabIdComputed = computed(() => props.activeTabId || activeTabIdFallback.value);
     const activeTab = computed(() => mySlotItems.value?.find(({ id }) => id === activeTabIdComputed.value));
+    const gridStylesClassNames = computed(() => {
+        if (props.isPreSlotActive && !props.isPostSlotActive) {
+            return 'vxda-tabs--is-pre-slot';
+        } else if (!props.isPreSlotActive && props.isPostSlotActive) {
+            return 'vxda-tabs--is-post-slot';
+        } else if (props.isPreSlotActive && props.isPostSlotActive) {
+            return 'vxda-tabs--is-pre-and-post-slot';
+        }
 
+        return 'vxda-tabs--is-no-slots';
+    });
+
+    const contentPadding = computed(() => contentHeight.value ? 'var(--vxda-tabs-content-padding)' : 0);
+    const breakpointsSorted = computed(() => {
+        if (props.breakpoints) {
+            const sortedKeys = Object.keys(props.breakpoints).sort((a, b) => a - b);
+
+            //todo move creating of media queries here so it can be fired on component load.
+            return sortedKeys.map((key) => ({ screenWidth: key, orientation: props.breakpoints?.[key] }));
+        }
+    });
 
     function setActiveTab(tabId: string) {
         emit('update:activeTabId', tabId);
         activeTabIdFallback.value = tabId;
     }
 
+    function attachMediaQueryEvents({ screenWidth, orientation }, index) {
+        const nextBreakpoint = breakpointsSorted.value[index + 1];
+        const min = `screen and (min-width: ${screenWidth}px)`;
+        const max = nextBreakpoint ? ` and (max-width: ${nextBreakpoint.screenWidth -1}px)` : '';
+
+
+        const mq = window.matchMedia(`${min}${max}`);
+
+        if (mq?.addEventListener) {
+            mq.addEventListener('change', (media) => {
+                if (media.matches) {
+                    currentOrientation.value = orientation;
+                }
+            });
+        } else {
+            mq.addListener(() => {
+                console.log('orientationqrwa', screenWidth, orientation);
+                currentOrientation.value = orientation;
+            });
+        }
+    }
+
+    function handleBreakpoints() {
+
+
+        breakpointsSorted.value.forEach(attachMediaQueryEvents);
+    }
+
     const resizeObserver = new ResizeObserver((entries) => {
         const entry = entries[0];
 
-        contentHeight.value = `${entry?.contentRect.height}px`;
+        contentHeight.value = entry?.contentRect.height ? `${entry?.contentRect.height}px` : 0;
     });
-
 
     onMounted(() => {
         if (!mySlotItems.value?.length && slots.default?.().length) {
@@ -118,6 +195,9 @@
                 activeTabIdFallback.value = mySlotItems.value?.[0].id;
             }
         }, 0);
+
+        handleBreakpoints();
+
     });
 
     onBeforeUnmount(() => {
@@ -130,55 +210,195 @@
 </script>
 
 <style lang="scss" scoped>
+
     .vxda-tabs {
         background-color: var(--vxda-tabs-backgound-color);
         width: 100%;
     }
 
+    .vxda-tabs__wrapper {
+        display: grid;
+        height: 100%;
+    }
+
+    .vxda-tabs__wrapper--horizontal {
+        grid-auto-rows: auto 1fr;
+    }
+
+    .vxda-tabs__wrapper--vertical {
+        grid-template-columns: min-content auto;
+        border-top: 1px solid var(--vxda-tabs-border-color);
+    }
+
+
     .vxda-tabs__nav {
         display: grid;
         grid-auto-flow: column;
-        grid-template-areas: 'pre tabs post';
-        grid-template-columns: 1fr 2fr 1fr;
         border-bottom: 1px solid var(--vxda-tabs-border-color);
         position: relative;
         margin: 0;
         padding: 0;
+
+        &.vxda-tabs__nav--vertical {
+            border-left: 1px solid var(--vxda-tabs-border-color);
+        }
+
+    }
+
+    .vxda-tabs--is-no-slots {
+        grid-template-areas: 'tabs';
+
+        &.vxda-tabs__nav--horizontal {
+            grid-template-columns: 1fr;
+        }
+
+        &.vxda-tabs__nav--vertical {
+            grid-template-rows: 1fr;
+        }
+    }
+
+    .vxda-tabs--is-pre-slot {
+        grid-template-areas: 'pre tabs';
+
+        &.vxda-tabs__nav--horizontal {
+            grid-template-columns: 1fr 2fr;
+        }
+
+        &.vxda-tabs__nav--vertical {
+            grid-template-rows: 1fr 2fr;
+        }
+    }
+
+    .vxda-tabs--is-post-slot {
+        grid-template-areas: 'tabs post';
+
+        &.vxda-tabs__nav--horizontal {
+            grid-template-columns: 2fr 1fr;
+        }
+
+        &.vxda-tabs__nav--vertical {
+            grid-template-rows: 2fr min-content;
+        }
+    }
+
+    .vxda-tabs--is-pre-and-post-slot {
+        grid-template-areas: 'pre tabs post';
+
+        &.vxda-tabs__nav--horizontal {
+            grid-template-columns: 1fr 2fr 1fr;
+        }
+
+        &.vxda-tabs__nav--vertical {
+            grid-template-rows: 1fr 2fr 1fr;
+        }
     }
 
     .vxda-tabs__buttons {
         grid-area: tabs;
+    }
+
+    .vxda-tabs__buttons--horizontal {
         display: flex;
         justify-content: v-bind(tabsAlignment);
     }
 
+    .vxda-tabs__buttons--vertical {
+        display: grid;
+        grid-auto-rows: 1fr;
+        //grid-template-rows: repeat(auto-fit, 1fr);
+    }
+
     .vxda-tabs__button {
-        border: 1px solid var(--vxda-tabs-border-color);
-        border-bottom-color: transparent;
+        border: 0;
         cursor: pointer;
         padding: 5px 10px;
         background-color: inherit;
-        border-left: 0;
         margin-bottom: -1px;
+        color: var(--vxda-tabs-nav-color);
+    }
+
+    .vxda-tabs__button--horizontal {
+        border: 1px solid var(--vxda-tabs-border-color);
+        border-bottom-color: transparent;
+        border-left: 0;
 
         &:first-child {
             border-left: 1px solid var(--vxda-tabs-border-color);
         }
     }
 
-    .vxda-tabs__buttons--active {
+    .vxda-tabs__button--vertical {
+        border-top: 1px solid var(--vxda-tabs-border-color);
+        border-right: 1px solid var(--vxda-tabs-border-color);
+
+        &:first-child {
+            border-top: 0;
+        }
+
+        &:last-child {
+            border-bottom: 0;
+        }
+    }
+
+    .vxda-tabs__button--active {
         position: relative;
-        z-index: 1;
-        border-bottom-color: var(--vxda-tabs-backgound-color);
+    }
+
+    .vxda-tabs__active-button-mask {
+        display: block;
+        position: absolute;
+        backdrop-filter: contrast(100);
+        backface-visibility: hidden;
+    }
+
+
+    .vxda-tabs__active-button-mask--horizontal {
+        width: 100%;
+        height: 10px;
+        bottom: -2px;
+        left: 0;
+    }
+
+    .vxda-tabs__active-button-mask--vertical {
+        height: 100%;
+        width: 1px;
+        right: -1px;
+        top: 0;
+        z-index: 10;
+        backdrop-filter: contrast(100);
     }
 
     .vxda-tabs__content {
-        border: 1px solid var(--vxda-tabs-border-color);
         border-top: 0;
-        height: v-bind(contentHeight);
-        transition: height .3s;
+        min-height: v-bind(contentHeight);
+        transition: min-height .3s, padding-top .3s, padding-bottom .3s;
         overflow: hidden;
         position: relative;
+        z-index: 2;
+        padding: v-bind(contentPadding);
+    }
+
+    .vxda-tabs__content--horizontal {
+        border: 1px solid var(--vxda-tabs-border-color);
+        border-top: 0;
+    }
+
+    .vxda-tabs__content--vertical {
+        border-right: 1px solid var(--vxda-tabs-border-color);
+        border-bottom: 1px solid var(--vxda-tabs-border-color);
+        border-left: 0;
+    }
+
+    .vxda-tabs__teleport-slots-vertical {
+        display: flex;
+
+        .vxda-tabs__slot-before {
+            margin-right: auto;
+        }
+
+        .vxda-tabs__slot-after {
+            margin-left: auto;
+        }
     }
 
     .fade-enter-active,
@@ -251,6 +471,10 @@
         opacity: 0;
         transform: translateY(50px);
     }
+
+    //.vxda-tabs__slot-before:empty {
+    //    display: none;
+    //}
 
 
 </style>
